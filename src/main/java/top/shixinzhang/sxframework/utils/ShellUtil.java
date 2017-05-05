40dd94b16b67edadda3ee5cb1bcba56c8dc68268
zhangshixin <zhangshixin@yaomaiche.com>
2017-05-05 23:16:00 +0800

    add file

---
+++ src/main/java/top/shixinzhang/sxframework/utils/ShellUtil.java
@@ -0,0 +1,15 @@
+package top.shixinzhang.sxframework.utils;
+
+/**
+ * Description:
+ * <br> ROOT 下执行 adb 命令的工具类
+ * <p>
+ * <br> Created by shixinzhang on 17/5/5.
+ * <p>
+ * <br> Email: shixinzhang2016@gmail.com
+ * <p>
+ * <a  href="https://about.me/shixinzhang">About me</a>
+ */
+
+public class ShellUtil {
+}
