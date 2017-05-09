7b358943a564842b96ad25f2cc92aa95ed919b57
zhangshixin <zhangshixin@yaomaiche.com>
2017-05-09 19:28:04 +0800

    add util again

--- README.md
+++ README.md
@@ -1,14 +1,24 @@
-# sxframework
+# sx-framework
 Some base function classes for android development. 
 
 从零开始创建自己的 Android 框架，主要包括：
 
 - network
-- imageloader
+ - 第三方网络库使用、分析
+ - 自己设计、封装 OkHttp
+- image
+ - 第三方图片加载库使用、分析
+ - 自己设计、封装图片加载库
 - cache
+ - 三级缓存的设计与分析
 - hybrid
+ - WebView 常见功能封装
+ - Weex 使用与分析
+ - 参考 Weex 创建 hybrid 组件
 - utils
+ - 常见工具类
 - views
+ - 常见布局
 
 目前阶段：
 
--- src/main/java/top/shixinzhang/sxframework/utils/ShellUtil.java
+++ src/main/java/top/shixinzhang/sxframework/utils/ShellUtil.java
@@ -1,5 +1,11 @@
 package top.shixinzhang.sxframework.utils;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+
 /**
  * Description:
  * <br> ROOT 下执行 adb 命令的工具类
@@ -12,4 +18,36 @@ package top.shixinzhang.sxframework.utils;
  */
 
 public class ShellUtil {
+
+    private static final String TAG = ShellUtil.class.getSimpleName();
+
+    public static void execCmd(String cmd) {
+        try {
+            // 申请获取root权限，这一步很重要，不然会没有作用
+            Process process = Runtime.getRuntime().exec("su");
+            // 获取输出流
+            OutputStream outputStream = process.getOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(
+                    outputStream);
+            dataOutputStream.writeBytes(cmd);
+            dataOutputStream.flush();
+            dataOutputStream.close();
+            outputStream.close();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    public static void testRoot(Context context) {
+        try {
+            Process su = Runtime.getRuntime().exec("su");
+            DataOutputStream outputStream = new DataOutputStream(su.getOutputStream());
+            outputStream.writeBytes("exit\n");
+            outputStream.flush();
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(context, "请允许ROOT权限", Toast.LENGTH_LONG).show();
+        }
+    }
 }
