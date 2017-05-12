0f5d785a7df4939043c55fc61427a0e0fa1ed877
zhangshixin <zhangshixin@yaomaiche.com>
2017-05-12 17:56:36 +0800

    add license

--- src/main/java/top/shixinzhang/sxframework/network/third/retrofit2/request/Utils.java
+++ src/main/java/top/shixinzhang/sxframework/network/third/retrofit2/request/Utils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2008 Google Inc.
+ * Copyright (c) 2017. shixinzhang (shixinzhang2016@gmail.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
